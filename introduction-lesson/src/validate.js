6fb0944a8972c940965fbd65d9674d8fc6f22890
prioarief <prioariefgunawangunawan@gmail.com>
2020-06-03 16:43:45 +0700

    task pertama

--- introduction-lesson/src/validate.js
+++ introduction-lesson/src/validate.js
@@ -1,4 +1,4 @@
 module.exports = {
-	usernameValidate: (username) => /[a-zA-Z0-9]{5,}$/.test(username),
+	usernameValidate: (username) => /[a-zA-Z0-9]{6,}$/.test(username),
 	passwordValidate: (password) => /^[a-zA-Z0-9!@#$%^&*()_+]{8}$/.test(password),
 };
